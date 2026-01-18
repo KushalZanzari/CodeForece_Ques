2ed84df9a95dec47aa684fc4c787a4066733f763
KushalZanzari <146122165+KushalZanzari@users.noreply.github.com>
2026-01-18 22:38:42 +0530

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Main {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        String word = sc.next();        System.out.print(Character.toUpperCase(word.charAt(0)));        System.out.println(word.substring(1));    }}
