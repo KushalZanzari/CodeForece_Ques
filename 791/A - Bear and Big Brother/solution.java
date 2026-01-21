606a0bce426bba169160eff5611d0f27b296e6b4
Kushal Zanzari <146122165+KushalZanzari@users.noreply.github.com>
2026-01-21 22:59:33 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Main {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        int limak = sc.nextInt();        int bob = sc.nextInt();                int years = 0;        while (limak <= bob) {            limak *= 3;            bob *= 2;            years++;        }        System.out.println(years);    }}
